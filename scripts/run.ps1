d87cbdfb4ad2382a470a4d2c7a575d3fbf08e83d
Serhii Kuklych <acidnyi@EVA-01.local>
2026-05-26 13:42:41 +0200

    add mongodb compose file

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -10,13 +10,28 @@ $ProjectRoot = "${PSScriptRoot}/.."
 
 $env:INCIDENT_LOG_API_ENVIRONMENT = "Development"
 $env:INCIDENT_LOG_API_PORT = "8080"
+$env:INCIDENT_LOG_API_MONGODB_USERNAME = "root"
+$env:INCIDENT_LOG_API_MONGODB_PASSWORD = "neUhaDnes"
+
+function mongo {
+    docker compose --file ${ProjectRoot}/deployments/docker-compose/compose.yaml $args
+}
 
 switch ($command) {
-    "start" {
-        go run ${ProjectRoot}/cmd/incident-log-api-service
-    }
     "openapi" {
         docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+    }
+    "start" {
+        try {
+            mongo up --detach
+            go run ${ProjectRoot}/cmd/incident-log-api-service
+        }
+        finally {
+            mongo down
+        }
+    }
+    "mongo" {
+        mongo up
     }
     default {
         throw "Unknown command: $command"
